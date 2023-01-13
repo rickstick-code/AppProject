13e9bb508299807291e56a6ceef0c1c43a1e8359
rickstick-code <64330538+rickstick-code@users.noreply.github.com>
2023-01-13 10:12:27 +0100

    test

---
+++ App/app/src/main/java/com/example/appproject/ui/resolutionList/testBranche.kt
@@ -0,0 +1,11 @@
+package com.example.appproject.ui.resolutionList
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class testBranche : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_branche)
+    }
+}

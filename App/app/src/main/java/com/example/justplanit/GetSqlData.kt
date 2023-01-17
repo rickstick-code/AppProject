4d93506dda0f6e6f542a5ab3fc6fcbaff2d698a5
rickstick-code <64330538+rickstick-code@users.noreply.github.com>
2023-01-17 15:57:41 +0100

    comparing

--- App/app/src/main/java/com/example/justplanit/GetSqlData.kt
+++ App/app/src/main/java/com/example/justplanit/GetSqlData.kt
@@ -3,6 +3,7 @@ package com.example.justplanit
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface GetSqlData {
@@ -11,4 +12,7 @@ interface GetSqlData {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insFortschritt(fortschritt: Fortschritt)
+
+    @Query("SELECT * FROM Fortschritt")
+    fun selFortschritt(): List<Fortschritt>
 }
--- App/app/src/main/java/com/example/justplanit/ui/progress/ProgressFragment.kt
+++ App/app/src/main/java/com/example/justplanit/ui/progress/ProgressFragment.kt
@@ -1,19 +1,14 @@
 package com.example.justplanit.ui.progress
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.justplanit.ProgressAdapter
-import com.example.justplanit.R
-import androidx.room.Room
 import com.example.justplanit.Fortschritt
 import com.example.justplanit.R
 import com.example.justplanit.SqlDatabase
@@ -42,12 +37,10 @@ class ProgressFragment : Fragment() {
 
 
         // create recycler view
-        /*
         val recyclerView = root.findViewById<RecyclerView>(R.id.progress_recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(activity)
         recyclerView.adapter = ProgressAdapter("Test")  //replace with progress class
 
-         */
 
         binding.progressAdd.setOnClickListener {
             SqlDatabase.getDatabase(requireContext().applicationContext).getSqlData.insFortschritt(
